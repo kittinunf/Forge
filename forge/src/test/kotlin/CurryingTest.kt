7666436de7338f4470e942e96900a56445e8a608
Kittinun Vantasin <kittinun.f@gmail.com>
2017-10-09 15:36:17 +0900

    Add More test

--- forge/src/test/kotlin/CurryingTest.kt
+++ forge/src/test/kotlin/CurryingTest.kt
@@ -25,13 +25,26 @@ class CurryingTest {
     fun testCurrying4() {
         val multiply = { w: Int, x: Int, y: Int, z: Int -> w * x * y * z }
         val curry = multiply.curry()
-        assertThat(curry(1)(2)(3)(4), equalTo(11))
+        assertThat(curry(1)(2)(3)(4), equalTo(24))
     }
 
     @Test
     fun testCurrying5() {
         val concat = { v: String, w: String, x: String, y: String, z: String -> v + w + x + y + z }
         assertThat(concat.curry()("a")("bb")("ccc")("dddd")("eeeee"), equalTo("abbcccddddeeeee"))
+    }
+
+    @Test
+    fun testCurrying6() {
+        val multiply = { u: Int, v: Int, w: Int, x: Int, y: Int, z: Int -> u * v * w * x * y * z }
+        val curry = multiply.curry()
+        assertThat(curry(1)(2)(3)(4)(5)(6), equalTo(720))
+    }
+
+    @Test
+    fun testCurrying7() {
+        val concat = { t: String, u: String, v: String, w: String, x: String, y: String, z: String -> t + u + v + w + x + y + z }
+        assertThat(concat.curry()("a")("bb")("ccc")("dddd")("eeeee")("f")("gg"), equalTo("abbcccddddeeeeefgg"))
     }
 
 }
