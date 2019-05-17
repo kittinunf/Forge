d862e7e6b8508daae175b4df104a617de7b37c6e
Kittinun Vantasin <kittinun.f@gmail.com>
2019-05-17 11:08:12 +0900

    Update the script

--- deploy_bintray.sh
+++ deploy_bintray.sh
@@ -4,12 +4,13 @@ if [[ "$TRAVIS_BRANCH" == */release-* ]]; then
 
   echo "We're on release branch, deploying at $TRAVIS_BRANCH"
 
-  modules=("forge")
-  for i in "${modules[@]}"
+  for i in $(ls -d */);
   do
-    echo ">> Deploying $i ..."
-    ./gradlew :$i:clean :$i:build :$i:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
-    echo ">> Done deploying for $i"
+    m=${i%%/}
+    if [[ $m == forge* ]]; then
+      echo ">> Deploying $m ..."
+      ./gradlew :$m:clean :$m:build :$m:bintrayUpload -PBINTRAY_USER=$BINTRAY_USER -PBINTRAY_KEY=$BINTRAY_KEY -PdryRun=false -Ppublish=true --no-configure-on-demand --no-parallel
+    fi
   done
 
 fi
