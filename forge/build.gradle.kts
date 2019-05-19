7f70f871f830ef8e7fb80352271ef016ab11cfb7
Kittinun Vantasin <kittinun.f@gmail.com>
2019-05-19 23:32:42 +0900

    We don't need this for now

--- forge/build.gradle.kts
+++ forge/build.gradle.kts
@@ -2,12 +2,10 @@ dependencies {
     val kotlinVersion: String by project
     val jsonVersion: String by project
     val junitVersion: String by project
-    val koptionalVersion: String by project
     val resultVersion: String by project
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("org.json:json:$jsonVersion")
-    implementation("com.gojuno.koptional:koptional:$koptionalVersion")
     implementation("com.github.kittinunf.result:result:$resultVersion")
 
     testImplementation("junit:junit:$junitVersion")
