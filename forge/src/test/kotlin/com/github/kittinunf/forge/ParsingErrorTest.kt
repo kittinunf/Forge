b5c8e2c7c0af2c4cd8fa0cc68ca202413441dd23
Kittinun Vantasin <kittinun.f@gmail.com>
2020-02-13 10:16:22 +0900

    Add error case test

--- forge/src/test/kotlin/com/github/kittinunf/forge/ParsingErrorTest.kt
+++ forge/src/test/kotlin/com/github/kittinunf/forge/ParsingErrorTest.kt
@@ -9,7 +9,10 @@ import com.github.kittinunf.forge.core.list
 import com.github.kittinunf.forge.core.map
 import com.github.kittinunf.forge.util.create
 import com.github.kittinunf.result.Result.Failure
+import com.github.kittinunf.result.Result.Success
+import com.github.kittinunf.result.lift
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
@@ -98,5 +101,39 @@ class ParsingErrorTest : BaseTest() {
 
         assertThat((user as Failure).error, instanceOf(AttributeTypeInvalidError::class.java))
         assertThat(user.error.message, equalTo("Attribute Type Invalid - key: \"levels\", expect type: class java.lang.Integer, received value: \"1\""))
+    }
+
+    @Test
+    fun testSomeInvalidTypeItems() {
+        val json = """
+        [
+            {
+                "name": "Clementina DuBuque",
+                "age": 46,
+                "levels" : ["3","2","1"]
+            },
+            {
+                "name": "DuBuque Clementina",
+                "age": 64,
+                "levels" : [3,2,1]
+            }
+        ]
+        """.trimIndent()
+
+        val users = Forge.modelsFromJson(json, User2.deserializer)
+        val (user0, user1) = users
+
+        assertThat((user0 as Failure).error, instanceOf(AttributeTypeInvalidError::class.java))
+        assertThat(user0.error.message, equalTo("Attribute Type Invalid - key: \"levels\", expect type: class java.lang.Integer, received value: \"3\""))
+
+        assertThat((user1 as Success).value, instanceOf(User2::class.java))
+        assertThat(user1.value.age, equalTo(64))
+        assertThat(user1.value.name, equalTo("DuBuque Clementina"))
+        assertThat(user1.value.levels, hasItems(1,2,3))
+
+        val liftedUser = users.lift()
+        assertThat((liftedUser as Failure).error, instanceOf(AttributeTypeInvalidError::class.java))
+        assertThat((liftedUser.error as AttributeTypeInvalidError).receivedValue as String, equalTo("3"))
+        assertThat(liftedUser.error.message, equalTo("Attribute Type Invalid - key: \"levels\", expect type: class java.lang.Integer, received value: \"3\""))
     }
 }
