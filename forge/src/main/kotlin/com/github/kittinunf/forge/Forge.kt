94ab96b787cda70b0ec0be9015f7639989d2f709
Kittinun Vantasin <kittinun.f@gmail.com>
2020-02-13 19:33:02 +0900

    Add more test to cover error class

--- forge/src/main/kotlin/com/github/kittinunf/forge/Forge.kt
+++ forge/src/main/kotlin/com/github/kittinunf/forge/Forge.kt
@@ -3,6 +3,7 @@ package com.github.kittinunf.forge
 import com.github.kittinunf.forge.core.Deserializable
 import com.github.kittinunf.forge.core.DeserializedResult
 import com.github.kittinunf.forge.core.JSON
+import com.github.kittinunf.result.lift
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -14,9 +15,9 @@ object Forge {
     fun <T : Any> modelFromJson(json: String, deserializer: JSON.() -> DeserializedResult<T>): DeserializedResult<T> =
             JSON.parse(JSONObject(json)).deserializer()
 
-    fun <T : Any, U : Deserializable<T>> modelsFromJson(json: String, deserializer: U): List<DeserializedResult<T>> =
-            JSON.parse(JSONArray(json)).toList().map(deserializer::deserialize)
+    fun <T : Any, U : Deserializable<T>> modelsFromJson(json: String, deserializer: U): DeserializedResult<List<T>> =
+            JSON.parse(JSONArray(json)).toList().map(deserializer::deserialize).lift()
 
-    fun <T : Any> modelsFromJson(json: String, deserializer: JSON.() -> DeserializedResult<T>): List<DeserializedResult<T>> =
-            JSON.parse(JSONArray(json)).toList().map(deserializer)
+    fun <T : Any> modelsFromJson(json: String, deserializer: JSON.() -> DeserializedResult<T>): DeserializedResult<List<T>> =
+            JSON.parse(JSONArray(json)).toList().map(deserializer).lift()
 }
--- forge/src/test/kotlin/com/github/kittinunf/forge/ParsingErrorTest.kt
+++ forge/src/test/kotlin/com/github/kittinunf/forge/ParsingErrorTest.kt
@@ -10,10 +10,7 @@ import com.github.kittinunf.forge.core.list
 import com.github.kittinunf.forge.core.map
 import com.github.kittinunf.forge.util.create
 import com.github.kittinunf.result.Result.Failure
-import com.github.kittinunf.result.Result.Success
-import com.github.kittinunf.result.lift
 import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
@@ -129,39 +126,5 @@ class ParsingErrorTest : BaseTest() {
         assertThat((error as AttributeTypeInvalidError).expectedClass, instanceOf(Class::class.java))
         assertThat(error.message, equalTo("Attribute Type Invalid - key: \"levels\", expected type: class java.lang.Integer, received value: \"3\""))
         assertThat(users, nullValue())
-    }
-
-    @Test
-    fun testSomeInvalidTypeItems() {
-        val json = """
-        [
-            {
-                "name": "Clementina DuBuque",
-                "age": 46,
-                "levels" : ["3","2","1"]
-            },
-            {
-                "name": "DuBuque Clementina",
-                "age": 64,
-                "levels" : [3,2,1]
-            }
-        ]
-        """.trimIndent()
-
-        val users = Forge.modelsFromJson(json, User2.deserializer)
-        val (user0, user1) = users
-
-        assertThat((user0 as Failure).error, instanceOf(AttributeTypeInvalidError::class.java))
-        assertThat(user0.error.message, equalTo("Attribute Type Invalid - key: \"levels\", expect type: class java.lang.Integer, received value: \"3\""))
-
-        assertThat((user1 as Success).value, instanceOf(User2::class.java))
-        assertThat(user1.value.age, equalTo(64))
-        assertThat(user1.value.name, equalTo("DuBuque Clementina"))
-        assertThat(user1.value.levels, hasItems(1,2,3))
-
-        val liftedUser = users.lift()
-        assertThat((liftedUser as Failure).error, instanceOf(AttributeTypeInvalidError::class.java))
-        assertThat((liftedUser.error as AttributeTypeInvalidError).receivedValue as String, equalTo("3"))
-        assertThat(liftedUser.error.message, equalTo("Attribute Type Invalid - key: \"levels\", expect type: class java.lang.Integer, received value: \"3\""))
     }
 }
